a22ec05b4dedee9d53d3a3d5682f7de190a1d902
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2021-06-06 21:12:49 +0200

    adding button component

---
+++ src/components/button.js
@@ -0,0 +1,6 @@
+import * as React from "react"
+import "./button.css"
+
+const Button = () => <button className="primary">Kliknij mnie</button>
+
+export default Button
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Button from "../components/button"
 
 const SecondPage = () => (
   <Layout>
@@ -10,6 +11,7 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <Button />
   </Layout>
 )
 
